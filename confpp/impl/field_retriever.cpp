1434484304c3b0069bcc357d4dc87181823761ae
Paolo Tranquilli <redsun82@gmail.com>
2024-02-25 19:09:00 +0100

    Rename field_indexer and remove buggy check

--- confpp/impl/field_retriever.cpp
+++ confpp/impl/field_retriever.cpp
@@ -1,16 +1,12 @@
-#include "confpp/impl/field_indexer.h"
+#include "confpp/impl/field_retriever.h"
 
 namespace confpp {
-void *FieldIndexer::index(size_t align, size_t size) {
+void *FieldRetriever::address(size_t align, size_t size) {
   size_t space = size * align; // upper limit for where the pointer could land
   auto res = std::align(align, size, current, space);
   assert(res);
   // set current just past this object
   current = reinterpret_cast<char *>(current) + size;
   return res;
-}
-
-bool FieldIndexer::indexed(void *address, size_t size) const {
-  return current == reinterpret_cast<char *>(address) + size;
 }
 } // namespace confpp
